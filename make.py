a55cfa28f51ccf5807a304f26d59c9e61d8a20c5
bill <bill.mill@gmail.com>
2012-05-10 00:08:07 -0400

    organizing make a bit

--- make.py
+++ make.py
@@ -1,46 +1,55 @@
 #!/usr/bin/env python
+from os import system
 from csv import reader
+from os.path import expanduser
+from pystache import render
 
 def intorzero(n):
     if not n: return 0
     else:     return int(n)
 
-totals = reader(open('total.csv', 'rb'))
+def create_team_page():
+    totals = reader(open('total.csv', 'rb'))
 
-#skip header
-totals.next()
-totals.next()
+    #skip header
+    totals.next()
+    totals.next()
 
-players = []
+    players = []
 
-for name, num, assists, goals, tos, ds, dnp in totals:
-    #break on the empty row before the sums
-    if not any([name, num, assists, goals, tos, ds, dnp]):
-        break
+    for name, num, assists, goals, tos, ds, dnp in totals:
+        #break on the empty row before the sums
+        if not any([name, num, assists, goals, tos, ds, dnp]):
+            break
 
-    num, assists, goals, tos, ds, dnp = map(intorzero,
-        [num, assists, goals, tos, ds, dnp])
+        num, assists, goals, tos, ds, dnp = map(intorzero,
+            [num, assists, goals, tos, ds, dnp])
 
-    players.append({
-        "name": name,
-        "number": num,
-        "assists": assists,
-        "goals": goals,
-        "turnovers": tos,
-        "blocks": ds,
-        "dnps": dnp
-    })
+        players.append({
+            "name": name,
+            "number": num,
+            "assists": assists,
+            "goals": goals,
+            "turnovers": tos,
+            "blocks": ds,
+            "dnps": dnp
+        })
 
-import pystache
+    import pystache
 
-out = file("test.html", "w")
-in_ = file("test.mustache.html").read()
+    out = file("test.html", "w")
+    in_ = file("test.mustache.html").read()
 
-out.write(pystache.render(in_, {"players": players}))
+    out.write(render(in_, {"players": players}))
+    out.close()
 
-out.close()
+    dropbox = expanduser("~/Dropbox/Public/ctconstitution/")
+    system("cp sorttable.js %s" % dropbox)
+    system("cp test.html %s" % dropbox)
 
-import os
-dropbox =  os.path.expanduser("~/Dropbox/Public/ctconstitution/")
-os.system("cp sorttable.js %s" % dropbox)
-os.system("cp test.html %s" % dropbox)
+def create_player_pages():
+    pass
+
+if __name__=="__main__":
+    create_team_page()
+    create_player_pages()
