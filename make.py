d58b39ecab16d8057863cb2005d81a4ffd42b06e
bill <bill.mill@gmail.com>
2012-05-10 22:29:29 -0400

    add playern page creation

--- make.py
+++ make.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 from os import system
 from csv import reader
-from os.path import expanduser
+from os.path import expanduser, isfile
 from pystache import render
+from datetime import datetime
 
 def intorzero(n):
     if not n: return 0
@@ -48,7 +49,63 @@ def create_team_page():
     system("cp test.html %s" % dropbox)
 
 def create_player_pages():
-    pass
+    players = {}
+    #for each game, grab the player records:
+    i = 1
+
+    filename = lambda i: "game%s.csv" % i
+
+    while isfile(filename(i)):
+        print "parsing game %s" % filename(i)
+
+        game = reader(open(filename(i), 'rb'))
+
+        meta = game.next()
+
+        teams = meta[0].split(" vs ")
+        if "onstitution" in teams[0]: ct, opponent = teams
+        else:                         opponent, ct = teams
+
+        date = datetime.strptime(meta[1], "%B %d %Y")
+
+        #skip header
+        game.next()
+
+        for row in game:
+            name, num, assists, goals, tos, ds = row[:6]
+
+            #break on the empty row before the sums
+            if not any([name, num, assists, goals, tos, ds]):
+                break
+
+            dnp = True if row[-1] == "DNP" else False
+
+            num, assists, goals, tos, ds = map(intorzero,
+                [num, assists, goals, tos, ds])
+
+            players.setdefault(name, []).append({
+                "opponent": opponent,
+                "date": date.strftime("%m/%d/%Y"),
+                "name": name,
+                "number": num,
+                "assists": assists,
+                "goals": goals,
+                "turnovers": tos,
+                "blocks": ds,
+                "dnp": dnp
+            })
+
+        i += 1
+
+    playerid = 0
+    for player, games in players.iteritems():
+        out = file("players/%s.html" % playerid, 'w')
+        in_ = file("player.mustache.html").read()
+
+        out.write(render(in_, {"player": player, "games": games}))
+        out.close()
+
+        playerid += 1
 
 if __name__=="__main__":
     create_team_page()
